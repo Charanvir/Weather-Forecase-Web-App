97c8004ebccdbad2d06a03713ae919d47832d0fc
Charanvir <charanvir123@gmail.com>
2023-01-14 02:33:34 -0500

    filtered data depending on user inputs

--- backend.py
+++ backend.py
@@ -10,9 +10,16 @@ def get_data(place, days, kind):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = 8 * days
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature":
+        filtered_data = [temp_dict["main"]["temp"] for temp_dict in filtered_data]
+    if kind == "Sky":
+        filtered_data = [sky_dict["weather"][0]["main"] for sky_dict in filtered_data]
+    return filtered_data
 
 
 if __name__ == "__main__":
-    sample_data = get_data("Toronto", "1", "1")
+    sample_data = get_data(place="Toronto", days=3, kind="Temperature")
     print(sample_data)
