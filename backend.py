b9928ee72c7a9290b4ebb6f80dba062caad801c0
Charanvir <charanvir123@gmail.com>
2023-01-14 03:23:13 -0500

    using api to show temperature data given cityname, days and condition. Showing images as well depending on api data

--- backend.py
+++ backend.py
@@ -6,20 +6,18 @@ load_dotenv()
 api_key = os.getenv("API_KEY")
 
 
-def get_data(place, days, kind):
+def get_data(place, days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [temp_dict["main"]["temp"] for temp_dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [sky_dict["weather"][0]["main"] for sky_dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    sample_data = get_data(place="Toronto", days=3, kind="Temperature")
-    print(sample_data)
+    sample_temp_data = get_data(place="Toronto", days=3)
+    sample_sky_data = get_data(place="Toronto", days=3)
+    print(sample_temp_data)
+    print(sample_sky_data)
--- main.py
+++ main.py
@@ -7,14 +7,33 @@ place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
 option = st.selectbox("Select data to view",
-                      ("Temperature", "Sky Conditions"))
+                      ("Temperature", "Sky"))
 if place:
     if days > 1:
         st.subheader(f"{option} for the next {days} days in {place}")
     else:
         st.subheader(f"{option} for the next day in {place}")
 
-    d, t = get_data(place, days, option)
+    try:
+        data = get_data(place, days)
 
-    figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-    st.plotly_chart(figure)
+        if option == "Temperature":
+            filtered_data = [temp_dict["main"]["temp"] for temp_dict in data]
+            temperature = [temp-271.15 for temp in filtered_data]
+            dates = [date_dict["dt_txt"] for date_dict in data]
+            figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            sky_conditions = [sky_dict["weather"][0]["main"] for sky_dict in data]
+            images = {
+                "Clear": "images/clear.png",
+                "Clouds": "images/cloud.png",
+                "Rain": "images/rain.png",
+                "Snow": "images/snow.png"
+            }
+            file_paths = [images[condition] for condition in sky_conditions]
+            st.image(file_paths, width=85)
+
+    except KeyError:
+        st.subheader("Please enter a valid city name to get forecast data")
