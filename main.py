b09cca83c207d9443ddae8a08941e4199de3d16f
Charanvir <charanvir123@gmail.com>
2023-01-13 19:51:55 -0500

    added line graph via the plotly package

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
-option = st.selectbox("Select tdata to view",
+option = st.selectbox("Select data to view",
                       ("Temperature", "Sky Conditions"))
-if place:
-    st.subheader(f"{option} for the next {days} days in {place}")
+while place:
+    if days > 1:
+        st.subheader(f"{option} for the next {days} days in {place}")
+    else:
+        st.subheader(f"{option} for the next day in {place}")
+
+    date = ["2022-25-10", "2022-26-10", "2022-27-10"]
+    temperature = [10, 11, 12]
+    figure = px.line(x=date, y=temperature, labels={"x": "Date", "y":"Temperature (C)"})
+    st.plotly_chart(figure)
